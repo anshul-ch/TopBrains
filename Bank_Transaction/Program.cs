239aab62bcae05969b1eb2f255cc627cd7e7817f
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-19 11:51:32 +0530

    Bank Tranaction simulation

---
+++ Bank_Transaction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        int initialBalance = int.Parse(input[0]);
+        int[] transactions = new int[input.Length - 1];
+        for (int i = 1; i < input.Length; i++)
+        {
+            transactions[i - 1] = int.Parse(input[i]);
+        }
+
+        int finalBalance = CalculateFinalBalance(initialBalance, transactions);
+        Console.WriteLine(finalBalance);
+    }
+
+    static int CalculateFinalBalance(int initialBalance, int[] transactions)
+    {
+        int balance = initialBalance;
+
+        foreach (int transaction in transactions)
+        {
+            if (transaction >= 0)
+            {
+                balance += transaction;
+            }
+            else if (balance + transaction >= 0)
+            {
+                balance += transaction;
+            }
+        }
+
+        return balance;
+    }
+}
