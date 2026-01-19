da1bdcdb52b885d28baa3e707e99c4938629ca9d
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-19 11:16:09 +0530

    Multiplication Table upto n times

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int upto = int.Parse(Console.ReadLine());
+        int[] result = GetRowMultiplication(n, upto);
+        Console.WriteLine("["+ String.Join(",", result)+"]");
+
+    }
+    
+    public static int[] GetRowMultiplication(int num, int upto)
+    {
+        if (upto <= 0)
+            return Array.Empty<int>();
+
+        int[] row = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            row[i - 1] = num * i;
+        }
+        return row;
+    }
+}
