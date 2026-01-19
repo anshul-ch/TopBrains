56588cbc6374a01e40eafbc94b87d7927537f8dd
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-19 11:43:58 +0530

    Height Category Display

---
+++ Display_Height/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double heightCm = double.Parse(Console.ReadLine());
+        string category = GetHeightCategory(heightCm);
+        Console.WriteLine(category);
+    }
+
+    static string GetHeightCategory(double heightCm)
+    {
+        if (heightCm < 150)
+            return "Short";
+        else if (heightCm < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+}
