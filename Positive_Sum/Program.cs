285164d70d174956f1beba11777db49820b0d7ce
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-22 14:35:41 +0530

    Sum of positive numbers in an array

---
+++ Positive_Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] numbers = { 1, -2, 3, 4, 0, -5, 6 };
+        int result = PositiveSum(numbers);
+        Console.WriteLine(result);
+    }
+    public static int PositiveSum(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            if (number > 0)
+            {
+                sum += number;
+            }
+            else if (number == 0)
+                break;
+        }
+        return sum;
+    }
+}
