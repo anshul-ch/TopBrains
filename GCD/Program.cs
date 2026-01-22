be698a8cac546fcf973a01ced9fdd256718d304c
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-22 15:06:10 +0530

    GCD of two non-negative integers

---
+++ GCD/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int a = 48;
+        int b = 18;
+        Console.WriteLine(Gcd(a, b));
+    }
+    static int Gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return Gcd(b, a % b);
+    }
+}
+
