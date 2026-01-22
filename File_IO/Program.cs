8e026bbc192fccc7772a3b2132af1689db0535a2
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-22 15:08:14 +0530

    File input/output

---
+++ File_IO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string inputPath = "log.txt";
+        string outputPath = "error.txt";
+
+        using (StreamReader reader = new StreamReader(inputPath))
+        using (StreamWriter writer = new StreamWriter(outputPath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains("ERROR"))
+                    writer.WriteLine(line);
+            }
+        }
+    }
+}
