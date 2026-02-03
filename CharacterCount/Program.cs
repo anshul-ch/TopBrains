bc9a793cc7a2f08abafad0d16555178fca85930f
anshul <anshulch.1606@gmail.com>
2026-02-03 15:05:03 +0530

    Count characters from String

---
+++ CharacterCount/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "hello world";
+
+        Dictionary<char, int> counts = CountCharacters(input);
+
+        foreach (var kvp in counts)
+        {
+            Console.WriteLine($"'{kvp.Key}': {kvp.Value}");
+        }
+    }
+
+    static Dictionary<char, int> CountCharacters(string input)
+    {
+        var result = new Dictionary<char, int>();
+
+        if (string.IsNullOrEmpty(input))
+            return result;
+
+        foreach (char c in input)
+        {
+            if (result.ContainsKey(c))
+                result[c]++;
+            else
+                result[c] = 1;
+        }
+
+        return result;
+    }
+}
