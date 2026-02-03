7121cb1863a923c255e5fc71ef2986e236b2d2f4
anshul <anshulch.1606@gmail.com>
2026-02-03 15:40:50 +0530

    Dictionaries

---
+++ Dictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Input
+        var salaries = new Dictionary<int, int>
+        {
+            { 1, 20000 },
+            { 4, 40000 },
+            { 5, 15000 }
+        };
+
+        int[] ids = { 1, 4, 5 };
+
+        int totalSalary = GetTotalSalary(ids, salaries);
+        Console.WriteLine(totalSalary);
+    }
+
+    static int GetTotalSalary(int[] ids, Dictionary<int, int> salaryByEmployeeId)
+    {
+        if (ids == null || salaryByEmployeeId == null)
+            return 0;
+
+        int total = 0;
+
+        foreach (int id in ids)
+        {
+            if (salaryByEmployeeId.TryGetValue(id, out int salary))
+            {
+                total += salary;
+            }
+        }
+
+        return total;
+    }
+}
