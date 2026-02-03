3bfc796eff44ab8b41fa98f3a6595cf3e46c03f7
anshul <anshulch.1606@gmail.com>
2026-02-03 15:39:23 +0530

    Array object

---
+++ Object_Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        object[] values =
+        {
+            10,
+            "hello",
+            true,
+            null,
+            25,
+            3.14,
+            5
+        };
+
+        int sum = SumIntegers(values);
+        Console.WriteLine($"Sum: {sum}");
+    }
+
+    static int SumIntegers(object[] values)
+    {
+        if (values == null || values.Length == 0)
+            return 0;
+
+        int sum = 0;
+
+        foreach (var item in values)
+        {
+            if (item is int x)
+            {
+                sum += x;
+            }
+        }
+
+        return sum;
+    }
+}
