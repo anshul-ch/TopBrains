ad98198987b307bd21c97c8266c04ee5c2d7a30f
anshul <anshulch.1606@gmail.com>
2026-02-03 15:02:41 +0530

    Conversion of lengths

---
+++ Conversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Sample inputs
+        Console.WriteLine(FeetToCentimeters(0));    // 0.00
+        Console.WriteLine(FeetToCentimeters(5));    // 152.40
+        Console.WriteLine(FeetToCentimeters(123));  // 3749.04
+    }
+
+    static double FeetToCentimeters(int feet)
+    {
+        const double CmPerFoot = 30.48;
+
+        double centimeters = feet * CmPerFoot;
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
+}
