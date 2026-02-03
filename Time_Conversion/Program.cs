6c4f23f5b2ae0f285fb74ff2863d66e6acb7a255
anshul <anshulch.1606@gmail.com>
2026-02-03 15:10:44 +0530

    time conversion

---
+++ Time_Conversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(FormatTime(125)); // 2:05
+        Console.WriteLine(FormatTime(60));  // 1:00
+        Console.WriteLine(FormatTime(9));   // 0:09
+    }
+
+    static string FormatTime(int totalSeconds)
+    {
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        return $"{minutes}:{seconds:D2}";
+    }
+}
